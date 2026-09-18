b2fcf4c2c326e961a27e83b756d10a437c37707e
deshanbandara4321-tech <deshanbandara4321@gmail.com>
2026-09-19 02:48:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://cloud.laksidu.site/dl/TDlzWsidKr/IMG-20260916-WA0006.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 𝖲𝖧𝖤𝖭-𝖬𝖣 Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
